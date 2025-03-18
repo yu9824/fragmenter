93e90616fd092178cf7a4ed257bb69094f41f148
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-03-18 20:46:56 +0900

    v0.0.1-rc.1

--- src/fragmenter/__init__.py
+++ src/fragmenter/__init__.py
@@ -1,6 +1,6 @@
 """Algorithm for automatically fragmenting molecules into predefined groups"""
 
-__version__ = "0.0.1-rc.0"
+__version__ = "0.0.1-rc.1"
 __license__ = "MIT"
 __author__ = "Simon and yu9824"
 __copyright__ = "Copyright (c) 2025 Simon and yu9824"
